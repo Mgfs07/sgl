7b0c248c7561f47e93de442fc138918d547b87c3
Fabio Formentini <fabio.formentini@basis.com.br>
2024-03-25 14:52:05 -0300

    Feat: implementar período

---
+++ sga-frontend/src/app/pages/local/periodo/periodo-form/periodo-form.component.ts
@@ -0,0 +1,73 @@
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {MessageService} from "primeng/api";
+import {DialogService, DynamicDialogConfig, DynamicDialogRef} from "primeng/dynamicdialog";
+import {PeriodoModel} from "../../../../shared/models/periodo.model";
+import {PeriodoService} from "../../../../shared/services/periodo.service";
+
+@Component({
+    selector: 'app-periodo-form',
+    templateUrl: './periodo-form.component.html',
+    styleUrls: ['./periodo-form.component.scss']
+})
+export class PeriodoFormComponent implements OnInit {
+    form: FormGroup;
+    periodo: PeriodoModel;
+    isEdit: boolean;
+    isVisualizar: boolean;
+
+    constructor(
+        private fb: FormBuilder,
+        private service: PeriodoService,
+        private messageService: MessageService,
+        public dialogService: DialogService,
+        private dialogConfig: DynamicDialogConfig,
+        public ref: DynamicDialogRef) {
+        this.definirFormulario();
+    }
+
+    ngOnInit() {
+        this.verificarAcao();
+    }
+
+    fecharDialog(periodoSalvo?: PeriodoModel) {
+        this.ref.close(periodoSalvo);
+    }
+
+    salvarPeriodo() {
+        const periodo = this.form.getRawValue();
+        this.service.insert(periodo).subscribe(value => {
+            this.fecharDialog(periodo)
+        })
+    }
+
+    private verificarAcao() {
+        if (this.dialogConfig.data.acao == 'visualizar') {
+            this.form.disable();
+            this.isVisualizar = true;
+        }
+        if (this.dialogConfig.data.acao == 'editar') {
+            this.form.enable();
+            this.isEdit = true;
+        }
+        this.renderizarDadosPeriodo();
+    }
+
+    private definirFormulario() {
+        this.form = this.fb.group({
+            id: [null],
+            descricao: [null, [Validators.required]],
+            dataInicio: [null, [Validators.required]],
+            dataFim: [null, [Validators.required]]
+        });
+    }
+
+    private renderizarDadosPeriodo() {
+        const periodoEncontrado = this.dialogConfig.data.periodo;
+        if (!periodoEncontrado) {
+            return;
+        }
+        this.form.patchValue(periodoEncontrado);
+    }
+
+}
---
+++ sga-frontend/src/app/pages/local/periodo/periodo.component.ts
@@ -0,0 +1,105 @@
+import {Component, OnInit} from '@angular/core';
+import {Column} from "../../../shared/models/colum.model";
+import {DialogService, DynamicDialogRef} from "primeng/dynamicdialog";
+import {ConfirmationService, MessageService} from "primeng/api";
+import {PeriodoService} from "../../../shared/services/periodo.service";
+import {PeriodoListModel} from "../../../shared/models/periodo-list.model";
+import {PeriodoFormComponent} from "./periodo-form/periodo-form.component";
+
+@Component({
+    selector: 'app-periodo',
+    templateUrl: './periodo.component.html',
+    styleUrls: ['./periodo.component.scss'],
+    providers: [DialogService, ConfirmationService, MessageService]
+})
+export class PeriodoComponent implements OnInit {
+
+    cols!: Column[];
+    periodos: PeriodoListModel[];
+    ref: DynamicDialogRef | undefined;
+    display: boolean = false;
+
+
+    constructor(public dialogService: DialogService,
+                private messageService: MessageService,
+                private service: PeriodoService,
+                private confirmationService: ConfirmationService) {
+    }
+
+    ngOnInit() {
+        this.construirColunasListagem();
+        this.buscarPeriodos()
+    }
+
+    private construirColunasListagem() {
+        this.cols = [
+            {field: 'descricao', header: 'Descrição', text: true},
+            {field: 'dataInicio', header: 'Início', text: true},
+            {field: 'dataFim', header: 'Fim', text: true},
+            {field: 'acoes', header: 'Ações'}
+        ];
+    }
+
+    private buscarPeriodos() {
+        this.service.findAll().subscribe((value: PeriodoListModel[]) => {
+            this.periodos = value;
+        })
+    }
+
+    novoPeriodo() {
+        this.ref = this.dialogService.open(PeriodoFormComponent,
+            {
+                header: 'Novo Período',
+                width: '18%',
+                data: {acao: ''}
+            });
+        this.ref.onClose.subscribe((periodo) => {
+            if (periodo) {
+                this.messageService.add({
+                    severity: 'success',
+                    summary: 'Success',
+                    detail: 'O período ' + periodo.nome + ' foi cadastrado com sucesso!'
+                })
+                this.buscarPeriodos();
+            }
+        });
+    }
+
+    handleAcao(rowData: any, acao: string) {
+        this.service.findById(rowData.id).subscribe((value) => {
+            this.ref = this.dialogService.open(PeriodoFormComponent,
+                {
+                    header: 'Formulário Período',
+                    width: '35%',
+                    data: {periodo: value, acao: acao}
+                });
+            this.ref.onClose.subscribe((periodo) => {
+                if (periodo) {
+                    this.messageService.add({
+                        severity: 'success',
+                        summary: 'Success',
+                        detail: 'O período ' + periodo.descricao + ' foi editado com sucesso!'
+                    })
+                    this.buscarPeriodos()
+                }
+            });
+        })
+    }
+
+    excluirPeriodo(id) {
+        this.confirmationService.confirm({
+            message: 'Tem certeza que deseja excluir o registro?',
+            header: 'Confirmação de Exclusão',
+            icon: 'pi pi-info-circle',
+            acceptLabel: 'Sim',
+            rejectLabel: 'Cancelar',
+            accept: () => {
+                this.service.delete(id).subscribe(() => {
+                    this.buscarPeriodos();
+                })
+                this.messageService.add({severity: 'info', summary: 'Confirmação', detail: 'Período inativado!'});
+            }
+        });
+    }
+
+}
