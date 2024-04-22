181d330b122f060a679f55650f7df9fe7f6aa33a
matheus <matheusantos07@hotmail.com>
2024-04-22 18:47:57 -0300

    Codificação de Alocao das aulas

--- Backend/src/main/java/com/br/sga/controller/AulaController.java
+++ Backend/src/main/java/com/br/sga/controller/AulaController.java
@@ -1,15 +1,13 @@
 package com.br.sga.controller;
 
 import com.br.sga.service.AulaService;
+import com.br.sga.service.dto.AulaDTO2;
 import com.br.sga.service.dto.DropdownDTO;
 import com.br.sga.service.dto.HorariosAula;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -29,5 +27,17 @@ public class AulaController {
     public ResponseEntity<List<DropdownDTO>> buscarTodos() {
         List<DropdownDTO> listagem = service.buscarDropdown();
         return new ResponseEntity<>(listagem, HttpStatus.OK);
+    }
+
+    @PostMapping("/alocar/{idLocal}/{idAula}")
+    public ResponseEntity<Void> salvarLocalAula(@PathVariable Long idLocal, @PathVariable Long idAula){
+        service.salvarLocalAula(idLocal, idAula);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping("/{idAula}")
+    public ResponseEntity<AulaDTO2> buscarAulaParaAlocacao(@PathVariable Long idAula){
+        AulaDTO2 aulaDTO2 = service.buscarAulaParaAlocacao(idAula);
+        return new ResponseEntity<>(aulaDTO2, HttpStatus.OK);
     }
 }
