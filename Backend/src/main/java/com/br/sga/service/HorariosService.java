875b469961b9243814b39937b699c2491464db4e
wagner.batista@basis.com.br <Wagner Bozzetti Batista>
2024-06-20 23:35:29 -0300

    Adicionando Filtro e Endpoint de Horarios da Aula

---
+++ Backend/src/main/java/com/br/sga/service/HorariosService.java
@@ -0,0 +1,34 @@
+package com.br.sga.service;
+
+import com.br.sga.service.dto.DropdownDTO;
+import com.br.sga.service.dto.FIltroDTO;
+import com.br.sga.service.dto.HorariosAula2;
+import com.br.sga.service.dto.Teste;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class HorariosService {
+    
+    private final TurmaService turmaService;
+    private final AulaService alunoService;
+
+    public List<DropdownDTO> buscarDropdownTurma() {
+        return turmaService.buscarDropdown();
+    }
+
+    public List<HorariosAula2> buscarHorariosAulaAluno2(String matricula) {
+        return alunoService.buscarHorariosAulaAluno2(matricula);
+    }
+
+    public List<Teste> buscarHorariosAula() {
+        return alunoService.buscarHorarios();
+    }
+
+
+}
